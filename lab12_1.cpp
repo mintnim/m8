a1ebbbf442b5126d3f80cfbdc04287a372839152
mintnim <minttotk@gmail.com>
2021-01-19 17:58:22 +0700

    complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,38 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[] , int N , double B[]){
+	for(int i = 1 ; i <= N ; i++){
+		B[0] += A[i - 1];
+	}
+	B[0] = B[0] / N;
+	
+	for(int j = 1 ; j <= N ; j++){
+		B[1] += pow(A[j - 1],2);
+	}
+	B[1] = sqrt((B[1] / N) - pow(B[0],2));
+	
+	B[2] = 1;
+	for(int k = 1 ; k <= N ; k++){
+		B[2] *= A[k - 1];
+	}
+	B[2] = pow(B[2] , 1.0 / N);
+	
+	for(int l = 1 ; l <= N ; l++){
+		B[3] += 1.0 / A[l - 1];
+	}
+	B[3] = N / B[3];
+	
+	B[4] = A[0];
+	B[5] = A[0];
+	for(int m = 1 ; m <= N ; m++){
+		if(A[m - 1] > B[4]){
+			B[4] = A[m - 1];
+		}
+		if(A[m - 1] < B[5]){
+			B[5] = A[m - 1];
+		}
+	}
+}
+
